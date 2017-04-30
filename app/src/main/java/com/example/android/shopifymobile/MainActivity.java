c693196e1412b9e2694a6c9f1c7f62f8fe4fa2ac
ac92 <alfredc_1092@hotmail.com>
2017-04-30 05:08:11 -0700

    Initialized text views

--- app/src/main/java/com/example/android/shopifymobile/MainActivity.java
+++ app/src/main/java/com/example/android/shopifymobile/MainActivity.java
@@ -5,11 +5,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTotalRevenue;
+    private TextView mKeyboardsSold;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mTotalRevenue = (TextView) findViewById(R.id.total_revenue);
+        mKeyboardsSold = (TextView) findViewById(R.id.total_keyboards_sold);
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
