dcfa047a1aa071f01202084a565af25c3f1b2ed0
ac92 <alfredc_1092@hotmail.com>
2017-05-01 02:48:29 -0700

    Button must be clicked to display data

--- app/src/main/java/com/example/android/shopifymobile/MainActivity.java
+++ app/src/main/java/com/example/android/shopifymobile/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.android.shopifymobile;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -38,14 +37,11 @@ public class MainActivity extends AppCompatActivity {
         mTotalRevenue = (TextView) findViewById(R.id.total_revenue);
         mKeyboardsSold = (TextView) findViewById(R.id.total_keyboards_sold);
 
-        fetchData();
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                fetchData();
             }
         });
     }
